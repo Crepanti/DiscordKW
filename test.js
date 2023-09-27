d732d619dcfa91f8b417a7a0fe40b3fa70e568ee
Crepanti <crepantidev@gmail.com>
2023-09-27 03:23:13 +0200

    up

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 function obliczWynik() {
-    var wynik = 3.16 * 3.16;
-    return wynik;
-  }
-  
-  var wynikMnozenia = obliczWynik();
+  var wynik = 3.16 * 3.16;
+  return wynik;
+}
+
+var wynikMnozenia = obliczWynik();
+wynikMnozenia;
